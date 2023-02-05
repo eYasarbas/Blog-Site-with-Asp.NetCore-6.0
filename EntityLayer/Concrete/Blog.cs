f8e9fc6b5e93a9e1a1d26526fa67a91c2038f88c
Enes Yaşarbaş <65676107+Huramiral2535@users.noreply.github.com>
2023-02-05 21:32:53 +0300

    fixed string error

--- EntityLayer/Concrete/Blog.cs
+++ EntityLayer/Concrete/Blog.cs
@@ -8,11 +8,11 @@ namespace EntityLayer.Concrete
     public class Blog
     {
         public int BlogID { get; set; }
-        public String BlogTitle { get; set; }
-        public String BlogContent { get; set; }
-        public String BlogImage { get; set; }
-        public String BlogThumbnailImage { get; set; }
-        public String BlogDate { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogImage { get; set; }
+        public string BlogThumbnailImage { get; set; }
+        public DateTime BlogDate { get; set; }
         public bool BlogStatus { get; set; }
     }
 }
---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public string CommentUserName { get; set; }
+        public string CommentTitle { get; set; }
+        public string CommentContent { get; set; }
+        public DateTime CommentDateTime { get; set; }
+        public bool CommentStatus { get; set; }
+    }
+}
